9b831b563710c54d0ad86d3179418250002197f0
Karthik <kn@beezlabs.com>
2022-04-22 08:43:55 +0530

    refactor

--- pkg/authorization/casbin/casbin.go
+++ pkg/authorization/casbin/casbin.go
@@ -3,6 +3,7 @@ package casbin
 import "github.com/beezlabs-org/go-microservices-lib/internal/authorization/casbin"
 
 var (
-	InitCasbinAndGetEnforcer = casbin.InitCasbinAndGetEnforcer
-	GetService               = casbin.GetService
+	InitCasbinAndGetEnforcer             = casbin.InitCasbinAndGetEnforcer
+	InitCasbinAndGetEnforcerWithCustomDB = casbin.InitCasbinAndGetEnforcerWithCustomDB
+	GetService                           = casbin.GetService
 )
